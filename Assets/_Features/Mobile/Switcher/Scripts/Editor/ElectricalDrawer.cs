4f1cb53672b5590e13d9c283bfdfea51f5274653
QuentinPietro <quentin.milana@hotmail.fr>
2020-10-31 18:31:28 +0100

    Tools Start

---
+++ Assets/_Features/Mobile/Switcher/Scripts/Editor/ElectricalDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ElectricalDrawingView))]
+public class ElectricalDrawer : Editor
+{
+
+    private void OnSceneGUI()
+    {
+        ElectricalDrawingView t = target as ElectricalDrawingView;
+         Event cur = Event.current;
+
+        //t.pos = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(cur.mousePosition);
+        t.pos = t.transform.position;
+        t.pos = new Vector3(t.pos.x, t.pos.y, 0);
+        Rect rect = new Rect(t.pos.x, t.pos.y, 1, 1);
+        Handles.DrawSolidRectangleWithOutline(rect, Color.cyan, Color.black);
+
+        if (cur.type == EventType.KeyDown)
+        {
+            t.isDrawing = true;
+        }
+
+
+    }
+}
---
+++ Assets/_Features/Mobile/Switcher/Scripts/ElectricalDrawingView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class ElectricalDrawingView : MonoBehaviour
+{
+    public GameObject[] GameObjects;
+    public Transform transform;
+    public Camera cam;
+    public Vector3 pos;
+    public bool isDrawing;
+    public RectTransform line;
+    public Canvas canvas;
+
+}
