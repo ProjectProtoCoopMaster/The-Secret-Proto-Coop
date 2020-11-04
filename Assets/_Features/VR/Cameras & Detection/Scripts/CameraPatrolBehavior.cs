0fa1b5b484fd2f95951d10eada1ee3d98ecacc88
Léonard <epoc4oftime@gmail.com>
2020-11-04 12:15:06 +0100

    Starting work on EntityPatrolData

---
+++ Assets/_Features/VR/Cameras & Detection/Scripts/CameraPatrolBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Gameplay.VR
+{
+    public class CameraPatrolBehavior : EntityPatrolData
+    {
+        // move the camera to its next patrol point location
+        IEnumerator MoveToAltPosition()
+        {
+            yield return null;
+        }
+    }
+}
