dcedf956c9af7ec9fc316408813f86cba98bba12
Léonard Gombert <epoc4oftime@gmail.com>
2020-11-09 23:48:06 +0100

    Working on Teleportation

--- Assets/_Features/VR/Player/Scripts/ShiftTeleportationBehavior.cs
+++ Assets/_Features/VR/Player/Scripts/ShiftTeleportationBehavior.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using Valve.VR;
 
 namespace Gameplay.VR.Player
 {
@@ -8,5 +10,33 @@ namespace Gameplay.VR.Player
     {
         // shift teleportation behavior
         [SerializeField] protected float movementSpeed;
+
+        [SerializeField] float minDashRange = 0.5f, maxDashRange = 40f, dashTime = 0.2f;
+
+        [SerializeField] Animator maskAnimator;
+
+        SteamVR_Action teleportAction;
+        Transform cameraRigRoot;
+
+        private void Start()
+        {
+            
+        }
+
+        IEnumerator Dashing()
+        {
+            float elapsed = 0f;
+            Vector3 startPoint = cameraRigRoot.position;
+
+            while (elapsed > dashTime)
+            {
+                elapsed += Time.deltaTime;
+                float elapsedPct = elapsed / Time;
+
+                cameraRigRoot.position = Vector3.Lerp(startPoint, )
+
+            }
+        }
+
     }
 }
