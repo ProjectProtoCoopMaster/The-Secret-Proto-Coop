65bbb7ff48592949d6bca9bae7e669b2b476ff93
Léonard <epoc4oftime@gmail.com>
2020-11-10 16:54:44 +0100

    Setup scene to test VR Shooting and Movement

--- Assets/_Features/VR/Player/Scripts/ShootBehavior.cs
+++ Assets/_Features/VR/Player/Scripts/ShootBehavior.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Valve.VR;
 
 namespace Gameplay.VR.Player
 {
     public class ShootBehavior : BallistixData
     {
-        Transform gunBarrel;
-        ProjectileBehavior projectile;
+        public Transform gunBarrel;
+        public ProjectileBehavior projectile;
 
         // a reference to the action
         public SteamVR_Action_Boolean shootAction;
@@ -18,16 +16,10 @@ namespace Gameplay.VR.Player
 
         private void Start()
         {
-            shootAction.AddOnStateDownListener(TriggerDown, handType);
-            shootAction.AddOnStateUpListener(TriggerUp, handType);
+            shootAction.AddOnStateUpListener(TriggerRelease, handType);
         }
 
-        private void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-        {
-
-        }
-
-        private void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+        private void TriggerRelease(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
         {
             projectile.transform.position = gunBarrel.position;
             projectile.transform.rotation = gunBarrel.rotation;
