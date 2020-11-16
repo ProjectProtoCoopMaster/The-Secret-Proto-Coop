8de21771e72a277cde0eed42faf6ca1fd3c7e678
Maxence <maxdurlon@gmail.com>
2020-11-16 21:35:00 +0100

    Small Fixes

--- Assets/_Features/AI/Scripts/TransformDebug.cs
+++ Assets/_Features/AI/Scripts/TransformDebug.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class TransformDebug : MonoBehaviour
 {
+    public bool usePath;
+    [ShowIf("usePath")]
     public PatrolPath path;
+
+    [SerializeField] private Color color;
+    //public Color Color { get { if (usePath) { path.pathColor; } else { color; } } set { } };
+
     public float radius;
 
     void OnDrawGizmos()
