bac9cd80001befcc404cf24609d2052f7e8f2a31
Léonard <epoc4oftime@gmail.com>
2020-11-16 23:23:49 +0100

    Player shooting behavior

---
+++ Assets/_Features/VR/Cameras/Scripts/GuardMortalityBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.VR
+{
+    public class GuardMortalityBehavior : MonoBehaviour
+    {
+        public void Shot()
+        {
+            gameObject.name = "DEAD";
+        }
+    }
+}
--- Assets/_Features/VR/Player/Scripts/ShootBehavior.cs
+++ Assets/_Features/VR/Player/Scripts/ShootBehavior.cs
@@ -6,7 +6,10 @@ namespace Gameplay.VR.Player
     public class ShootBehavior : BallistixData
     {
         public Transform gunBarrel;
-        public ProjectileBehavior projectile;
+        //public ProjectileBehavior projectile;
+        public LayerMask shootingLayer;
+
+        RaycastHit hit;
 
         // a reference to the action
         public SteamVR_Action_Boolean shootAction;
@@ -19,12 +22,26 @@ namespace Gameplay.VR.Player
             shootAction.AddOnStateUpListener(TriggerRelease, handType);
         }
 
+        private void Update()
+        {
+            Debug.DrawRay(gunBarrel.position, transform.forward * 50f, Color.magenta);
+
+            if (Physics.Raycast(gunBarrel.position, gunBarrel.transform.forward, out hit, shootingLayer))
+            {
+                if (hit.collider.gameObject.CompareTag("Enemy"))
+                    hit.collider.GetComponent<GuardMortalityBehavior>().Shot();
+            }
+        }
+
         private void TriggerRelease(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
         {
-            projectile = FindObjectOfType<ProjectileBehavior>();
-            projectile.transform.position = gunBarrel.position;
-            projectile.transform.rotation = gunBarrel.rotation;
-            projectile.Shoot();
+            Debug.DrawRay(gunBarrel.position, transform.forward * 50f, Color.magenta);
+
+            if (Physics.Raycast(gunBarrel.position, transform.forward, out hit, shootingLayer))
+            {
+                if(hit.collider.gameObject.CompareTag("Enemy")) 
+                    GetComponent<GuardMortalityBehavior>().Shot();
+            }
         }
     }
 }
